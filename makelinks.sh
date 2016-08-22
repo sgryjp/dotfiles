6146a1b1171196f88902106a52b8e06b70477d60
Suguru YAMAMOTO <sgryjp@gmail.com>
2016-08-22 16:29:40 +0900

    Ignore ".DS_Store"

--- makelinks.sh
+++ makelinks.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 function makelinks() {
-    scriptdir=$(dirname $0)
-    srcdir=$(cd $scriptdir/$1; pwd)
+    scriptdir=`dirname $0`
+    srcdir=`cd $scriptdir/$1; pwd`
     destdir=$HOME/$1
 
-    for fname in $(find $srcdir -type f -name "$2"); do
-        fname=$(basename $fname)
+    fnames=`find "$srcdir" -type f -name "$2" | grep -v ".DS_Store"`
+    for fname in $fnames; do
+        fname=`basename $fname`
 	if [ -h $destdir/$fname ]; then
             continue 
         fi
         mkdir -p $destdir
+        echo $destdir/$fname
         ln -s $srcdir/$fname $destdir/$fname
     done
 }
