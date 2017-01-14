6c83dfa7864bf8aa4b80ce18abdf3bbe26c2f3df
Suguru YAMAMOTO <sgryjp@gmail.com>
2017-01-14 22:11:17 +0900

    Add shell.vim

--- makelinks.sh
+++ makelinks.sh
@@ -8,13 +8,13 @@ function makelinks() {
     fnames=`find "$srcdir" -type f -name "$2" | grep -v ".DS_Store"`
     for fname in $fnames; do
         fname=`basename $fname`
-	if [ -h $destdir/$fname ]; then
-            continue 
-        fi
+        #if [ -h $destdir/$fname ]; then
+        #    continue
+        #fi
         mkdir -p $destdir
         destdir=`cd $destdir; pwd`      # for cleaner message
         echo $destdir/$fname
-        ln -s $srcdir/$fname $destdir/$fname
+        ln -fs $srcdir/$fname $destdir/$fname
     done
 }
 
