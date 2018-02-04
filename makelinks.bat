5f681ff4d59c327e3c76604c3a730d416722567f
Suguru YAMAMOTO <sgryjp@gmail.com>
2018-02-04 13:29:04 +0900

    Update makelinks.bat

--- makelinks.bat
+++ makelinks.bat
@@ -1,42 +1,49 @@
 @echo off
 setlocal EnableExtensions
 
-REM ---------------------------------------------------------------------------
-set ROOT=%~dp0
+set BATDIR=%~dp0
 if not exist "%USERPROFILE%" (
     echo [Error] Environment variable %%USERPROFILE%% is not set.
     goto :CLEANUP
 )
 
-REM ---------------------------------------------------------------------------
-pushd "%ROOT%"
-
 REM Check access priviledge
-if exist ".\makelinks.tmp" del ".\makelinks.tmp"
-(mklink ".\makelinks.tmp" "%~0" 2>&1) > NUL
+if exist "%TEMP%\makelinks.tmp" del "%TEMP%\makelinks.tmp"
+(mklink "%TEMP%\makelinks.tmp" "%~0" 2>&1) > NUL
 if not %ERRORLEVEL% == 0 (
-    echo Administrator priviledge needed.
+    echo Failed to create symbolic link
     goto :CLEANUP
 )
+del ".\makelinks.tmp" 2> NUL
 
-call :MKDIR  "%USERPROFILE%\vimfiles"
-call :MKLINK "%USERPROFILE%\vimfiles\vimrc" "%ROOT%\.vim\vimrc"
-
-:CLEANUP
-del ".\makelinks.tmp" > NUL
-popd
+call :MAIN "%USERPROFILE%\vimfiles\"
 goto :EOF
 
+
 REM ---------------------------------------------------------------------------
+:MAIN
+    set DOTVIM=%~f1
+    call :MKDIR  "%DOTVIM%"
+    call :MKLINK "%DOTVIM%vimrc" "%BATDIR%.vim\vimrc"
+    call :MKDIR  "%DOTVIM%ftplugin"
+    for %%I in (%BATDIR%.vim\ftplugin\*.vim) do (
+        call :MKLINK "%DOTVIM%ftplugin\%%~nxI" "%%~fI"
+    )
+    exit /b 0
+
 :MKLINK
-    if exist "%~1" del /q "%~1"
-    mklink "%~1" "%~2" > NUL
-    echo New link: %~1
+    set DEST=%~f1
+    set SRC=%~f2
+    if exist "%DEST%" del /q "%DEST%"
+    mklink "%DEST%" "%SRC%" > NUL
+    echo mklink "%DEST%" "%SRC%"
     exit /b 0
 
 :MKDIR
-    if not exist "%~1" (
-        mkdir "%~1"
-        echo New dir:  %~1
+    if not exist "%~f1" (
+        mkdir "%~f1"
+	echo mkdir "%~f1"
+    ) else (
+        echo mkdir "%~f1" ^(skip^)
     )
     exit /b 0
