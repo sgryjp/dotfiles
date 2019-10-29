741cbbac37bc605e7df62fe913178fff5f1b43ab
Suguru YAMAMOTO <sgryjp@gmail.com>
2019-10-29 17:07:00 +0900

    profile: Use XDG compliant structure
    .bashrc and .profile will be sourced from the site-specific original
    one.

--- makelinks.sh
+++ makelinks.sh
@@ -5,16 +5,35 @@ SCRIPT_PATH=$(dirname "$SCRIPT")
 makelink() {
     if test "$(readlink -f $2)" != "$1"; then
         ln -fsv "$1" "$2"
+        return 0
     fi
+    return 1
 }
 
-for F in bashrc gitconfig gitignore inputrc profile; do
-    makelink $SCRIPT_PATH/$F ~/.$F
+# Files to be replaced
+mkdir -vp ~/.config/git
+for F in gitconfig gitignore; do
+    makelink $SCRIPT_PATH/$F ~/.config/git/$F
+done
+makelink $SCRIPT_PATH/inputrc ~/.inputrc
+
+# Files to be additionally sourced
+mkdir -vp ~/.local/etc
+for F in bashrc profile; do
+    makelink $SCRIPT_PATH/$F ~/.local/etc/$F
+    if test $? = 0; then
+        if test $(grep "~/.local/etc/$F" ~/.$F | wc -l) = 0; then
+            printf 'if test -e "~/.local/etc/%s"; then ' "$F" >> ~/.$F
+            printf 'source "~/.local/etc/%s"; fi\n' "$F" >> ~/.$F
+        fi
+    fi
 done
 
+# VIM
 mkdir -vp ~/.vim/autoload
 makelink $SCRIPT_PATH/vimfiles/vimrc             ~/.vim/vimrc
 makelink $SCRIPT_PATH/vimfiles/autoload/plug.vim ~/.vim/autoload/plug.vim
 
+# Neovim
 mkdir -vp ~/.config/nvim/
 makelink $SCRIPT_PATH/init.vim ~/.config/nvim/init.vim
