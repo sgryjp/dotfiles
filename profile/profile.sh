a75ab22d9143390ac854732b524c2f2ecb806fd2
Suguru YAMAMOTO <sgryjp@gmail.com>
2020-04-29 20:53:34 +0900

    profile: Activate nodenv if available

--- profile/profile.sh
+++ profile/profile.sh
@@ -5,4 +5,8 @@ export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
 export PATH=$HOME/bin:$HOME/.local/bin:$PATH
 
+if command -v nodenv > /dev/null; then
+    eval "$(nodenv init -)"
+fi
+
 umask 022
