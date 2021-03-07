ca187a2b7d0a4c5019caa0f7a2bd9f8264d48efd
Suguru Yamamoto <sgryjp@gmail.com>
2021-03-07 09:59:56 +0900

    pwsh: Add PowerShell profile

---
+++ pwsh/profile.ps1
@@ -0,0 +1,17 @@
+# "dot source" this file in my $PROFILE
+
+# Define git related functions for aliases
+function GitDiff { git diff }
+function GitLog { git log --graph --oneline --decorate }
+function GitShowBranch { git show-branch }
+function GitStatus { git status -s }
+
+# Define git related aliases. Note, "gl" is for Get-Location by default.
+Set-Alias gd GitDiff
+Set-Alias gb GitShowBranch
+Set-Alias gs GitStatus
+if ($Host.Version.Major -ge 7) {
+    Set-Alias -Force gl GitLog
+} else {
+    Set-Alias -Force -Option AllScope gl GitLog
+}
