a00250940c32811aa833e1de3c1f84dae8bf9e20
Suguru Yamamoto <sgryjp@gmail.com>
2022-01-22 17:04:27 +0900

    Revert "profile: Split env.sh from profile.sh"
    This reverts commit 841c008bd9dafea3e1c0f9b432b56bc162568e19.

--- profile/profile.sh
+++ profile/profile.sh
@@ -1,1 +1,12 @@
+export LANG='en_US.UTF-8'
+export LC_ALL='en_US.UTF-8'
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+export PATH=$HOME/bin:$HOME/.local/bin:$PATH
+
 umask 022
+
+if test -x ~/.linuxbrew/bin/brew; then
+    eval $(~/.linuxbrew/bin/brew shellenv)
+fi
