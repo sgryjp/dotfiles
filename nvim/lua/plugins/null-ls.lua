e69c2dfa430857a1cfc584c27036f0efe9f7d58e
Suguru Yamamoto <sgryjp@gmail.com>
2023-05-12 09:18:36 +0900

    vim: Do not use flake8 by default

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -6,7 +6,7 @@ end
 null_ls.setup {
     sources = {
         null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.flake8,
+        -- null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.formatting.prettier.with({
             filetypes = { "html", "json", "yaml", "markdown" },
         }),
