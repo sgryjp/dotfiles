260a5bbf7ddda573bc969fa0c886819a7807ae49
Suguru Yamamoto <sgryjp@gmail.com>
2023-05-12 09:14:12 +0900

    vim: Use telescope-ui-select.nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,4 +13,13 @@ telescope.setup {
             },
         },
     },
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+                -- even more opts
+            }
+        }
+    }
 }
+
+require("telescope").load_extension("ui-select")
