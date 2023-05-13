7e717fab83ce923dc3428119a332bb6815424d85
Suguru Yamamoto <sgryjp@gmail.com>
2023-05-13 21:32:39 +0900

    vim: Use markdownlint through null-ls

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -7,8 +7,11 @@ null_ls.setup {
     sources = {
         null_ls.builtins.formatting.black,
         -- null_ls.builtins.diagnostics.flake8,
+
         null_ls.builtins.formatting.prettier.with({
             filetypes = { "html", "json", "yaml", "markdown" },
         }),
+
+        null_ls.builtins.diagnostics.markdownlint,
     }
 }
