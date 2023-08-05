8e9db84d3f0f373d0c3c97d649021441322b0bb1
Suguru Yamamoto <sgryjp@gmail.com>
2023-08-05 15:29:59 +0900

    vim: Use mini.cursorword

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,7 +1,12 @@
 local ok
-local mini_surround
+local mini_surround, mini_cursorword
 
 ok, mini_surround = pcall(require, "mini.surround")
 if ok then
 	mini_surround.setup {}
+end
+
+ok, mini_cursorword = pcall(require, "mini.cursorword")
+if ok then
+	mini_cursorword.setup {}
 end
