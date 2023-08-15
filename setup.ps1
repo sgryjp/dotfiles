800715a79829132b82d3769f03eb934da4335c48
Suguru Yamamoto <sgryjp@gmail.com>
2023-08-15 16:04:47 +0900

    pwsh: Overhaul Windows setup script

--- setup.ps1
+++ setup.ps1
@@ -2,26 +2,61 @@
 [CmdletBinding()]
 param ()
 
+$ErrorActionPreference = "Stop" # Stop immediately whatever an error occurred
+
+# Ensure XDG_CONFIG_HOME is defined
+if ($null -eq $env:XDG_CONFIG_HOME) {
+    Write-Error "Environment variable XDG_CONFIG_HOME is not set."
+    exit 3
+}
+
 function New-Directory {
+    [CmdletBinding()]
     param (
-        [string]$path
+        [string]$Path
     )
-    $path = "$env:USERPROFILE\$path"
-    Write-Verbose "mkdir $path"
-    if (-not (Test-Path -Path $path -PathType Container)) {
-        New-Item -ItemType Directory $path
+    Write-Verbose "(dir)  $Path"
+    if (Test-Path -Path $Path -PathType Leaf) {
+        Write-Warning "Cannot make a directory because the target exists: $Path"
+    }
+    elseif (-not (Test-Path -Path $Path -PathType Container)) {
+        $null = New-Item -ErrorAction $ErrorActionPreference -ItemType Directory $Path
     }
 }
 
 function New-SymlinkFile {
+    [CmdletBinding()]
     param (
-        [string]$target
+        [string]$LinkTarget,
+        [string]$Path
     )
-    $target = "$PSScriptRoot\$target"
-    $path = $env:USERPROFILE + "\" + $args[0]
-    Write-Verbose "ln -s $target $path"
-    if (-not (Test-Path -Path $path)) {
-        New-Item -Verbose -ItemType SymbolicLink -Path $path -Target $target
+    Write-Verbose "(link) $Path -> $LinkTarget"
+    if (-not (Test-Path -Path $Path)) {
+        $null = New-Item -ErrorAction $ErrorActionPreference `
+            -Verbose `
+            -ItemType SymbolicLink `
+            -Path $Path `
+            -Target $LinkTarget
+    }
+}
+
+function Get-LinkPathAndTarget {
+    [CmdletBinding()]
+    param (
+        [string]$Source,
+        [string]$Destination
+    )
+    return Get-ChildItem -File -Recurse $Source | ForEach-Object {
+        try {
+            Push-Location $Source
+            $target = $_
+            $path = Join-Path $Destination (Resolve-Path -Relative $_)
+            $path = $path.Replace("\.\", "\")
+            return [Tuple]::Create($target, $path)
+        }
+        finally {
+            Pop-Location
+        }
     }
 }
 
@@ -29,26 +64,45 @@ function New-SymlinkFile {
 git config --global include.path $PSScriptRoot\git\config
 Copy-Item "$PSScriptRoot\git\ignore" "$env:USERPROFILE\.gitignore"
 
-# VIM configurations
-New-Directory "_vim\autoload"
-New-SymLinkFile -Target "vimfiles\vimrc"                "_vim\vimrc"
-New-SymlinkFile -Target "vimfiles\autoload\plug.vim"    "_vim\autoload\plug.vim"
-New-Directory ".vim\autoload"
-New-SymlinkFile -Target "vimfiles\vimrc"                ".vim\vimrc"
-New-SymlinkFile -Target "vimfiles\autoload\plug.vim"    ".vim\autoload\plug.vim"
-New-Directory "vimfiles\autoload"
-New-SymlinkFile -Target "vimfiles\vimrc"                "vimfiles\vimrc"
-New-SymlinkFile -Target "vimfiles\autoload\plug.vim"    "vimfiles\autoload\plug.vim"
-
-# Neovim configurations
-New-Directory ".config\nvim\autoload"
-New-Directory ".config\nvim\lua\plugins"
-Get-ChildItem -Recurse -File "nvim" | %{
-    $target = $_.FullName
-    $relpath = Resolve-Path -Relative -LiteralPath $target
-    $relpath = $relpath.TrimStart(".\\")  # ".\nvim\***" --> "nvim\***"
-    $source = Join-Path ".config" $relpath
-    Invoke-Expression "New-SymlinkFile -Target '$relpath' '$source'"
+# Create directories
+$dirs = [System.Collections.ArrayList]::new(
+    @(
+        # VIM ------------------------------------------------------------------
+        "$env:USERPROFILE\_vim",
+        "$env:USERPROFILE\_vim\autoload",
+        "$env:USERPROFILE\.vim",
+        "$env:USERPROFILE\.vim\autoload",
+        "$env:USERPROFILE\vimfiles",
+        "$env:USERPROFILE\vimfiles\autoload",
+        # Neovim ---------------------------------------------------------------
+        "$env:XDG_CONFIG_HOME\nvim",
+        "$env:XDG_CONFIG_HOME\nvim\autoload"
+    )
+)
+Get-ChildItem -Recurse -Directory -Path "$PSScriptRoot\nvim" | ForEach-Object {
+    $path = Join-Path $env:XDG_CONFIG_HOME (Resolve-Path -Relative -Path $_)
+    $path = $path.Replace("\.\", "\")
+    $null = $dirs.Add($path)
 }
-New-SymlinkFile -Target "vimfiles\vimrc"                ".config\nvim\vimrc"
-New-SymlinkFile -Target "vimfiles\autoload\plug.vim"    ".config\nvim\autoload\plug.vim"
+$dirs | ForEach-Object { New-Directory $_ }
+
+# Create symbolic links
+$links = [ordered]@{  # <link file to create> = <link target>
+    # VIM
+    "$env:USERPROFILE\_vim\vimrc"                 = "$PSScriptRoot\vimfiles\vimrc";
+    "$env:USERPROFILE\_vim\autoload\plug.vim"     = "$PSScriptRoot\vimfiles\autoload\plug.vim";
+    "$env:USERPROFILE\.vim\vimrc"                 = "$PSScriptRoot\vimfiles\vimrc";
+    "$env:USERPROFILE\.vim\autoload\plug.vim"     = "$PSScriptRoot\vimfiles\autoload\plug.vim";
+    "$env:USERPROFILE\vimfiles\vimrc"             = "$PSScriptRoot\vimfiles\vimrc";
+    "$env:USERPROFILE\vimfiles\autoload\plug.vim" = "$PSScriptRoot\vimfiles\autoload\plug.vim";
+    # Neovim
+    "$env:XDG_CONFIG_HOME\nvim\vimrc"             = "$PSScriptRoot\vimfiles\vimrc";
+    "$env:XDG_CONFIG_HOME\nvim\autoload\plug.vim" = "$PSScriptRoot\vimfiles\autoload\plug.vim";
+}
+(Get-LinkPathAndTarget -Source "$PSScriptRoot\nvim" -Destination "$env:XDG_CONFIG_HOME\nvim") `
+| ForEach-Object {
+    $target = $_[0]
+    $path = $_[1]
+    $links[$path] = $target
+}
+$links.GetEnumerator() | ForEach-Object { New-SymlinkFile -LinkTarget $_.Value -Path $_.Key }
