25e2c853dd7abd2764ad8086d91ba18864807dbb
Suguru Yamamoto <sgryjp@gmail.com>
2023-08-15 12:19:28 +0900

    pwsh: Fix linter warning

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -22,14 +22,14 @@ Get-ChildItem Alias: | Where-Object -Property Name -in @("curl", "wget") | Remov
 # Use Emacs keybindings
 Set-PSReadLineOption -EditMode Emacs
 
-# Use standard-ish prompt except that user profile directory is shown as "~" 
+# Use standard-ish prompt except that user profile directory is shown as "~"
 function prompt {
     "PS " + "$(Get-Location)".Replace($env:USERPROFILE, "~") + "> "
 }
 
 # Check and warn if host specific environment variables are not as expected
-if ($env:XDG_CONFIG_HOME -eq $null) { Write-Warning "XDG_CONFIG_HOME is not defind." }
-if ($env:XDG_DATA_HOME -eq $null)   { Write-Warning "XDG_DATA_HOME is not defind." }
+if ($null -eq $env:XDG_CONFIG_HOME) { Write-Warning "XDG_CONFIG_HOME is not defind." }
+if ($null -eq $env:XDG_DATA_HOME)   { Write-Warning "XDG_DATA_HOME is not defind." }
 
 # Setup EDITOR environment variable
 if (Get-Command -CommandType Application nvim) {
