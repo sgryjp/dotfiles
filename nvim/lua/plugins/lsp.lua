ac830cb7d74256e9ea404d16b8a899b4abd92e3b
Suguru Yamamoto <sgryjp@gmail.com>
2024-01-20 20:32:13 +0900

    vim: Let lua_ls recognize global `vim`

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,5 +13,23 @@ mason_lspconfig.setup {
 
 mason_lspconfig.setup_handlers({ function(server_name)
     local opts = {}
+    if server_name == 'lua_ls' then
+        local libpath = {}
+        table.insert(libpath, vim.fn.stdpath("config") .. "/lua")
+        table.insert(libpath, vim.env.VIMRUNTIME .. "/lua")
+        opts = {
+            settings = {
+                Lua = {
+                    runtime = "LuaJIT",
+                    diagnostics = {
+                        globals = { 'vim' },
+                    },
+                    workspace = {
+                        library = libpath,
+                    },
+                }
+            }
+        }
+    end
     lspconfig[server_name].setup(opts)
 end });
