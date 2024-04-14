ccdadb91fa2068d2285c51cf97cce4aceef630b1
Suguru Yamamoto <sgryjp@gmail.com>
2024-04-14 23:37:13 +0900

    vim: Enable mini.surround on VSCode

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,19 +4,17 @@ vim.cmd("let &packpath=&runtimepath")
 vim.cmd("runtime vimrc")
 
 -- Load plugins config files in filename order.
-local function load_plugins()
-    local plugin_dir = vim.fn.stdpath("config") .. "/lua/plugins"
-    local plugins = vim.fn.readdir(plugin_dir)
-    table.sort(plugins)
+local plugin_dir = vim.fn.stdpath("config") .. "/lua/plugins"
+local plugins = vim.fn.readdir(plugin_dir)
+table.sort(plugins)
 
-    for _, plugin in ipairs(plugins) do
-        if plugin:match("%.lua$") then
+for _, plugin in ipairs(plugins) do
+    if plugin:match("%.lua$") then
+        if not vim.g.vscode or plugin:match("%mini.lua$") then
             require("plugins/" .. plugin:gsub("%.lua$", ""))
         end
     end
 end
 
-if not vim.g.vscode then
-    load_plugins()
-end
+-- Load neovim only keymaps
 require("keymaps")
--- nvim/lua/plugins/00_mini.lua
+++ nvim/lua/plugins/00_mini.lua
@@ -1,85 +1,87 @@
 local ok
 local clue, completion, extra, notify, surround, cursorword, indentscope, statusline, pick
 
-ok, completion = pcall(require, "mini.completion")
-if ok then
-    completion.setup({})
-end
+if vim.g.vscode then
+    ok, surround = pcall(require, "mini.surround")
+    if ok then
+        surround.setup({})
+    end
+else
+    ok, completion = pcall(require, "mini.completion")
+    if ok then
+        completion.setup({})
+    end
 
-ok, clue = pcall(require, "mini.clue")
-if ok then
-    clue.setup({
-        window = {
-            delay = 250,
-        },
-        triggers = {
-            { mode = "n", keys = "\\" },
-            { mode = "x", keys = "\\" },
-            { mode = "n", keys = "<Space>" },
-            { mode = "x", keys = "<Space>" },
-            { mode = "i", keys = "<C-x>" },
-            { mode = "n", keys = "g" },
-            { mode = "x", keys = "g" },
-            { mode = "n", keys = "'" },
-            { mode = "n", keys = "`" },
-            { mode = "x", keys = "'" },
-            { mode = "x", keys = "`" },
-            { mode = "n", keys = '"' },
-            { mode = "x", keys = '"' },
-            { mode = "i", keys = "<C-r>" },
-            { mode = "c", keys = "<C-r>" },
-            { mode = "n", keys = "<C-w>" },
-            { mode = "n", keys = "z" },
-            { mode = "x", keys = "z" },
-            { mode = "n", keys = "[" },
-            { mode = "n", keys = "]" },
-            -- gitsigns
-            { mode = "n", keys = "<C-g>" },
-        },
-        clues = {
-            clue.gen_clues.builtin_completion(),
-            clue.gen_clues.g(),
-            clue.gen_clues.marks(),
-            clue.gen_clues.registers(),
-            clue.gen_clues.windows(),
-            clue.gen_clues.z(),
-        },
-    })
-end
+    ok, clue = pcall(require, "mini.clue")
+    if ok then
+        clue.setup({
+            window = {
+                delay = 250,
+            },
+            triggers = {
+                { mode = "n", keys = "\\" },
+                { mode = "x", keys = "\\" },
+                { mode = "n", keys = "<Space>" },
+                { mode = "x", keys = "<Space>" },
+                { mode = "i", keys = "<C-x>" },
+                { mode = "n", keys = "g" },
+                { mode = "x", keys = "g" },
+                { mode = "n", keys = "'" },
+                { mode = "n", keys = "`" },
+                { mode = "x", keys = "'" },
+                { mode = "x", keys = "`" },
+                { mode = "n", keys = '"' },
+                { mode = "x", keys = '"' },
+                { mode = "i", keys = "<C-r>" },
+                { mode = "c", keys = "<C-r>" },
+                { mode = "n", keys = "<C-w>" },
+                { mode = "n", keys = "z" },
+                { mode = "x", keys = "z" },
+                { mode = "n", keys = "[" },
+                { mode = "n", keys = "]" },
+                -- gitsigns
+                { mode = "n", keys = "<C-g>" },
+            },
+            clues = {
+                clue.gen_clues.builtin_completion(),
+                clue.gen_clues.g(),
+                clue.gen_clues.marks(),
+                clue.gen_clues.registers(),
+                clue.gen_clues.windows(),
+                clue.gen_clues.z(),
+            },
+        })
+    end
 
-ok, extra = pcall(require, "mini.extra")
-if ok then
-    extra.setup({})
-end
+    ok, extra = pcall(require, "mini.extra")
+    if ok then
+        extra.setup({})
+    end
 
-ok, notify = pcall(require, "mini.notify")
-if ok then
-    notify.setup({})
-    vim.notify = notify.make_notify()
-end
+    ok, notify = pcall(require, "mini.notify")
+    if ok then
+        notify.setup({})
+        vim.notify = notify.make_notify()
+    end
 
-ok, surround = pcall(require, "mini.surround")
-if ok then
-    surround.setup({})
-end
+    ok, cursorword = pcall(require, "mini.cursorword")
+    if ok then
+        cursorword.setup({})
+    end
 
-ok, cursorword = pcall(require, "mini.cursorword")
-if ok then
-    cursorword.setup({})
-end
+    ok, indentscope = pcall(require, "mini.indentscope")
+    if ok then
+        indentscope.setup({})
+    end
 
-ok, indentscope = pcall(require, "mini.indentscope")
-if ok then
-    indentscope.setup({})
-end
+    ok, statusline = pcall(require, "mini.statusline")
+    if ok then
+        statusline.setup({})
+    end
 
-ok, statusline = pcall(require, "mini.statusline")
-if ok then
-    statusline.setup({})
-end
-
-ok, pick = pcall(require, "mini.pick")
-if ok then
-    pick.setup({})
-    vim.ui.select = pick.ui_select
+    ok, pick = pcall(require, "mini.pick")
+    if ok then
+        pick.setup({})
+        vim.ui.select = pick.ui_select
+    end
 end
