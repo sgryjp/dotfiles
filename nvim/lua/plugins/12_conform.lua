3c7c6d77e4cb4edda2db8a674ec3916de5a8da0c
Suguru Yamamoto <sgryjp@gmail.com>
2025-02-11 14:38:45 +0900

    vim: Update conform.nvim config scheme

--- nvim/lua/plugins/12_conform.lua
+++ nvim/lua/plugins/12_conform.lua
@@ -7,14 +7,14 @@ conform.setup({
     -- Conform will run multiple formatters sequentially.
     -- Use a sub-list to run only the first available formatter.
     formatters_by_ft = {
-        html = { { "prettierd", "prettier" } },
-        json = { { "prettierd", "prettier" } },
+        html = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
         lua = { "stylua" },
-        markdown = { { "prettierd", "prettier" } },
+        markdown = { "prettierd", "prettier", stop_after_first = true },
         python = { "ruff_format" },
         sh = { "shfmt" },
         swift = { "swift_format" },
-        yaml = { { "prettierd", "prettier" } },
+        yaml = { "prettierd", "prettier", stop_after_first = true },
     },
     log_level = vim.log.levels.INFO,
 })
