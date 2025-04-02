ea5a3a0da0bddb0d8ac4f26a3d1fedeff862da3b
Suguru Yamamoto <sgryjp@gmail.com>
2025-04-03 01:00:33 +0900

    vim: Use virtual_lines

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -76,7 +76,8 @@ else
 end
 if vim.version.ge(nvim_version, { 0, 11 }) then
   vim.diagnostic.config({
-    virtual_text = { current_line = true },
+    virtual_text = true,
+    virtual_lines = { current_line = true },
   })
 end
 
