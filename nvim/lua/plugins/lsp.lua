e1e39fcd790d592dc75e6e2893d6dac219b078a4
Suguru Yamamoto <sgryjp@gmail.com>
2025-05-10 01:02:18 +0900

    nvim: Migrate LSP config for mason.nvim 2.0

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,57 +9,32 @@ end
 -- Make omnifunc LSP based completion
 vim.omnifunc = "v:lua.vim.lsp.omnifunc"
 
-mason.setup({})
--- mason_lspconfig.setup {
---     automatic_installation = true
--- }
-
-local function configure_server(server_name)
-  local opts = {}
-  if server_name == "lua_ls" then
-    opts = {
-      settings = {
-        Lua = {
-          runtime = "LuaJIT",
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME,
-              "${3rd}/luv/library",
-            },
-          },
+-- Set default configuration for LSP clients
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      runtime = "LuaJIT",
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          "${3rd}/luv/library",
         },
       },
-    }
-  elseif server_name == "typos_ls" then
-    opts = {
-      cmd_env = { RUST_LOG = "info" },
-      init_options = {
-        diagnosticSeverity = "hint",
-      },
-    }
-  end
-  lspconfig[server_name].setup(opts)
-end
-
--- Define list of servers which I prefer to use
-local configured_servers = {
-  nushell = false,
-  -- pyright = false,
-}
-
--- Setup servers managed by mason.nvim and then manually setup the rest
-mason_lspconfig.setup_handlers({
-  function(server_name)
-    configure_server(server_name)
-    configured_servers[server_name] = true
-  end,
+    },
+  },
 })
-for server_name, installed in pairs(configured_servers) do
-  if not installed then
-    configure_server(server_name)
-  end
-end
+vim.lsp.config("typos_ls", {
+  cmd_env = { RUST_LOG = "info" },
+  init_options = {
+    diagnosticSeverity = "hint",
+  },
+})
+
+-- Enable LSP servers
+mason.setup({})
+mason_lspconfig.setup({})
+vim.lsp.enable("nushell")
