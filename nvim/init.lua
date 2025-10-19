ebbdb6c32bd29057ebb6d570269fafdfbe2340f6
Suguru Yamamoto <sgryjp@gmail.com>
2025-10-20 08:08:07 +0900

    nvim: update mini.nvim org to nvim-mini

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,24 @@
 -- Install mini.nvim and then setup mini.deps
 local function install_mini_deps()
-  local path_package = vim.fn.stdpath("data") .. "/site/"
-  local mini_path = path_package .. "pack/deps/start/mini.nvim"
+  local path_package = vim.fn.stdpath("data") .. "/site"
+  local mini_path = path_package .. "/pack/deps/start/mini.nvim"
   if not vim.loop.fs_stat(mini_path) then
     vim.cmd('echo "Installing `mini.nvim`" | redraw')
     local clone_cmd = {
       "git",
       "clone",
       "--filter=blob:none",
-      "https://github.com/echasnovski/mini.nvim",
+      -- Uncomment next line to use 'stable' branch
+      "--branch",
+      "stable",
+      "https://github.com/nvim-mini/mini.nvim",
       mini_path,
     }
     vim.fn.system(clone_cmd)
     vim.cmd("packadd mini.nvim | helptags ALL")
     vim.cmd('echo "Installed `mini.nvim`" | redraw')
   end
+
   local deps = require("mini.deps")
   deps.setup({ path = { package = path_package } })
   return deps
