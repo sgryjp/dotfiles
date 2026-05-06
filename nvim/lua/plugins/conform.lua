89873d2008f2975ab7de0c3bdcbb2d1ba215087a
Suguru Yamamoto <sgryjp@gmail.com>
2026-05-07 08:54:09 +0900

    nvim: Use oxfmt & oxlint

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,17 +7,22 @@ conform.setup({
   -- Conform will run multiple formatters sequentially.
   -- Use a sub-list to run only the first available formatter.
   formatters_by_ft = {
-    html = { "prettierd", "prettier", stop_after_first = true },
-    json = { "prettierd", "prettier", stop_after_first = true },
+    css = { "oxfmt" },
+    less = { "oxfmt" },
+    html = { "oxfmt", "prettier" },
+    json = { "oxfmt", "prettier" },
+    javascriptreact = { "oxfmt", "oxlint" },
     lua = { "stylua" },
-    markdown = { "prettierd", "prettier", stop_after_first = true },
+    markdown = { "oxfmt", "prettier" },
     nu = { "nufmt" },
     python = { "ruff_format" },
     rust = { "trim_whitespace", "rustfmt", lsp_format = "fallback" },
     sh = { "shfmt" },
     swift = { "swift_format" },
-    toml = { "taplo" },
-    yaml = { "prettierd", "prettier", stop_after_first = true },
+    toml = { "oxfmt", "taplo" },
+    typescript = { "oxfmt", "oxlint" },
+    typescriptreact = { "oxfmt", "oxlint" },
+    yaml = { "oxfmt", "oxlint", "prettier" },
   },
   log_level = vim.log.levels.INFO,
 })
