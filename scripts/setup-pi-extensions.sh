24a294940b75caac3a56cb000739bb7034e358ac
Suguru Yamamoto <sgryjp@gmail.com>
2026-09-24 13:10:17 +0900

    pi: update pi-tool-renderer to 2.0.2

--- scripts/setup-pi-extensions.sh
+++ scripts/setup-pi-extensions.sh
@@ -7,7 +7,7 @@ set -- \
   "npm:cache-warm@0.2.0" \
   "npm:@gotgenes/pi-subagents@21.7.6" \
   "npm:@tmustier/pi-usage-extension@0.9.4" \
-  "npm:@vanillagreen/pi-tool-renderer@1.7.1"
+  "npm:@vanillagreen/pi-tool-renderer@2.0.1"
 
 for extension in "$@"; do
   pi install "$extension"
